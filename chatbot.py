552f27eab04e0c1633a1ad6ba7359d763f1cb94d
gonzo <calleriza.gonzalo@gmail.com>
2023-11-15 13:11:04 +0100

    Creating a bag of words for association

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,6 @@
 import json
 import nltk
+from nltk.stem.lancaster import LancasterStemmer
 
 nltk.download('punkt')
 # Connect with database
@@ -21,3 +22,27 @@ for intent in data["intents"]:
         
         if intent["tag"] not in classes:
             classes.append(intent["tag"])
+            
+# Cleaning data
+stemmer = LancasterStemmer()
+
+# This will fraction the sentences in lower case
+words = [stemmer.stem(word.lower()) for word in words]
+
+# We sort the fractions, make them a set to remove duplicates and transform it into a list
+words = sorted(list(set(words)))
+
+# Build a bag of words for our ML model
+empty_output = [0] * len(classes)
+training_data = []
+
+for document in documents:
+    bag_of_words = []
+    pattern_words = document[0]
+    pattern_words = [stemmer.stem(word.lower()) for word in pattern_words]
+    for word in words:
+        bag_of_words.append(1) if word in pattern_words else bag_of_words.append(0)
+    
+    output_row = list(empty_output)
+    output_row[classes.index(document[1])] = 1
+    training_data.append([bag_of_words, output_row])
